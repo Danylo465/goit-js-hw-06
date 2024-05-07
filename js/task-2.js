963599c8c44e041f8d387f16143a6ba6ade60661
Danylo465 <152861478+Danylo465@users.noreply.github.com>
2024-05-07 20:54:16 +0300

    scnd cmmt jshw

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,21 @@
 class Storage {
-    constructor(initialItems) {
-      this.items = initialItems || []; 
-    }
-  
-    getItems() {
-      return this.items;
-    }
-  
-    addItem(newItem) {
-      this.items.push(newItem); 
-    }
-  
-    removeItem(itemToRemove) {
-      this.items = this.items.filter(item => item !== itemToRemove); 
-    }
+  constructor(initialItems) {
+    this.#items = initialItems || []; 
   }
+
+  getItems() {
+    return this.#items; 
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem); 
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove); 
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
